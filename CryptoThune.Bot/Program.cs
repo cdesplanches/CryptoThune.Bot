6ad040da47e6c1ddb22d34633a8d245afae90a11
Christophe Desplanches <kcdesplanches@gmail.com>
2020-10-08 21:48:22 -0400

    Initial BB

---
+++ CryptoThune.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptoThune.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
