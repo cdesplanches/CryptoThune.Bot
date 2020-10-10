211bff42fecf0fd2c8ad4c0fe61e43001140dcfb
Christophe Desplanches <kcdesplanches@gmail.com>
2020-10-10 15:11:42 -0400

    Initial BB

--- CryptoThune.Bot/Program.cs
+++ CryptoThune.Bot/Program.cs
@@ -1,12 +1,88 @@
 ﻿using System;
+using CommandLine;
+using Cryptothune.Net;
+using NLog;
+
 
 namespace CryptoThune.Bot
 {
+    /// <summary>
+    /// Main
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        private static readonly Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// Options thqt can passed to the progrqm
+        /// </summary>
+        public class Options
         {
-            Console.WriteLine("Hello World!");
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+
+            [Option('s', "sim", Required = false, HelpText = "Launch a simulation.")]
+            public bool Simulate { get; set; }
+
+            [Option('d', "dry", Default = false, Required = false, HelpText = "Run.")]
+            public bool DryRun { get; set; }
+        }
+        /// <summary>
+        /// The entry point
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static int Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                        Console.WriteLine("BotRunner v=1.0");
+
+                        var config = new NLog.Config.LoggingConfiguration();
+
+                        // Targets where to log to: Console
+                        var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+                        config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+                        NLog.LogManager.Configuration = config;
+
+                        if (o.Verbose)
+                        {
+                            Console.WriteLine($"Verbosity: ON");
+                        }
+
+                        if ( o.Simulate )
+                        {
+                            var bot = new BotThune<ExchangeFake>();
+                            bot.MarketExchange.Deposit(295.0);
+                            var strategy = new Funiol(1.0, 7.0, 0.6);
+                            bot.AddStrategy(strategy, "XTZEUR", 20.0 );
+                            bot.AddStrategy(strategy, "XRPEUR", 80.0 );
+                            bot.Sim(startDate: new DateTime(2020, 09, 07));
+                        }
+                        else
+                        {
+                             var bot = new BotThune<ExchangeKraken>();
+                            var strategy = new Funiol(1.0, 7.0, 0.6);
+                            bot.AddStrategy(strategy, "XTZEUR", 20.0);
+                            bot.AddStrategy(strategy, "BTCEUR", 5.0);
+                            bot.AddStrategy(strategy, "XRPEUR", 75.0 );
+                            if ( o.DryRun )
+                            {
+                                bot.DryRun();
+                            }
+                            else    // Oh; real run
+                            {
+                                bot.Run();
+                            }
+                            
+                        }
+
+
+                   });
+
+
+                   return 0;
+
         }
     }
 }
